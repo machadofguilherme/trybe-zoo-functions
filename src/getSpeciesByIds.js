114346bace805d86e0cb241993ed891bb99ac314
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-28 21:49:48 -0300

    Requisito 1.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,36 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function variousArguments(...ids) {
+  ids.forEach((element) => {
+    const manyParameters = species.filter((e) => element === e.id)
+      .map((e) => e.name);
+    return manyParameters;
+  });
 }
+
+function oneArgument(...ids) {
+  const oneID = species
+    .filter((element, index) => element.id === ids[index])
+    .filter((element) => element.name).map((element) => element.name);
+  return oneID;
+}
+
+function getSpeciesByIds(...ids) {
+  // seu código aqui
+  if (ids.length === 0) { return []; }
+
+  if (ids.length === 1) {
+    oneArgument(...ids);
+  } else {
+    variousArguments(...ids);
+  }
+}
+
+getSpeciesByIds(
+  '0938aa23-f153-4937-9f88-4858b24d6bce',
+  'baa6e93a-f295-44e7-8f70-2bcdc6f6948d',
+  '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae',
+  '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+);
 
 module.exports = getSpeciesByIds;
