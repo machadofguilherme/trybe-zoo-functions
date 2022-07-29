3dd15054f0ed7cc9e7efe84b4b11e70c6e30c993
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-29 14:52:01 -0300

    Requisito 2.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,16 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
+function getAnimalsOlderThan(animal, animalAge) {
   // seu código aqui
+  const objFiltered = species.filter((element) => element.name === animal)
+    .filter((element) => element.residents)
+    .find((element) => element.residents.sort((a, b) => a.age - b.age));
+  const { residents: [{ age }] } = objFiltered;
+
+  if (animalAge <= age) { return true; } return false;
 }
+
+getAnimalsOlderThan('penguins', 10);
 
 module.exports = getAnimalsOlderThan;
