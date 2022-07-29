a98721b5db743b99492467e6d83ab85ce84a1c59
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-29 16:30:04 -0300

    Requisito 3.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,22 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+
+function program(employeeName) {
+  const employeeData = employees.find((element) => {
+    const first = employeeName === element.firstName;
+    const second = employeeName === element.lastName;
+    return first || second;
+  });
+
+  return employeeData;
+}
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const exec = (employeeName === undefined) ? {} : program(employeeName);
+  return exec;
 }
+
+getEmployeeByName('Wishart');
 
 module.exports = getEmployeeByName;
