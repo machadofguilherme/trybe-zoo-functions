4b28a5c122b1158394d0d70e26e9cdec5800fb99
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-29 11:44:23 -0300

    Requisito 1.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,36 +1,41 @@
 const { species } = require('../data/zoo_data');
 
+function data(e) {
+  const animalData = {
+    id: e.id,
+    name: e.name,
+    popularity: e.popularity,
+    location: e.location,
+    availability: e.availability,
+    residents: e.residents.map((e2) => {
+      const parentsData = { name: e2.name, age: e2.age, sex: e2.sex };
+      return parentsData;
+    }),
+  };
+  return animalData;
+}
+
 function variousArguments(...ids) {
-  ids.forEach((element) => {
-    const manyParameters = species.filter((e) => element === e.id)
-      .map((e) => e.name);
-    return manyParameters;
-  });
+  const [primeiro, outros] = ids;
+  return species.filter((element) => element.id === primeiro || element.id === outros)
+    .filter((e) => data(e));
 }
 
 function oneArgument(...ids) {
-  const oneID = species
-    .filter((element, index) => element.id === ids[index])
-    .filter((element) => element.name).map((element) => element.name);
-  return oneID;
+  return species
+    .filter((element) => element.id === ids[0]).map((e) => data(e));
 }
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
   if (ids.length === 0) { return []; }
-
-  if (ids.length === 1) {
-    oneArgument(...ids);
-  } else {
-    variousArguments(...ids);
-  }
+  if (ids.length === 1) { return oneArgument(...ids); }
+  if (ids.length >= 2) { return variousArguments(...ids); }
 }
 
 getSpeciesByIds(
   '0938aa23-f153-4937-9f88-4858b24d6bce',
-  'baa6e93a-f295-44e7-8f70-2bcdc6f6948d',
-  '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae',
-  '89be95b3-47e4-4c5b-b687-1fabf2afa274',
+  'e8481c1d-42ea-4610-8e11-1752cfc05a46',
 );
 
 module.exports = getSpeciesByIds;
