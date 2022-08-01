41922aa12f7fb803a9416a1590fd2a8b2daae817
Guilherme Machado <machadofguilherme@gmail.com>
2022-07-31 23:28:31 -0300

    Requisito 12.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,38 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function objectData(animalList, locationList, searchEmployee) {
+  return searchEmployee.filter((element) => {
+    const objeto = ({
+      id: element.id,
+      fullName: `${element.firstName} ${element.lastName}`,
+      species: animalList,
+      locations: locationList,
+    });
+    console.log(objeto);
+    return objeto;
+  });
 }
+
+function getEmployeesCoverage(employeeData) {
+  // seu código aqui
+  const { name, id } = employeeData;
+
+  const searchEmployee = employees
+    .filter((element) => element.firstName === name
+      || element.lastName === name || element.id === id)
+    .filter((element) => element.responsibleFor);
+
+  const dataList = searchEmployee[0].responsibleFor;
+  const animalList = dataList.map((element) => species
+    .find((e) => e.id === element)).map((e) => e.name);
+
+  const locationList = dataList.map((element) => species
+    .find((e) => e.id === element)).map((e) => e.location);
+
+  return objectData(animalList, locationList, searchEmployee);
+}
+
+getEmployeesCoverage({ name: 'Sharonda' });
 
 module.exports = getEmployeesCoverage;
